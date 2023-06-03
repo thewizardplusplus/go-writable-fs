8fd194bf482b621cf12b10ca786ffb13234cfa90
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-06-03 14:31:36 +0600

    Add the `DirFS.Mkdir()` method

--- dir_fs.go
+++ dir_fs.go
@@ -3,6 +3,9 @@ package writablefs
 import (
 	"io/fs"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 type DirFS struct {
@@ -19,4 +22,35 @@ func NewDirFS(baseDir string) DirFS {
 
 func (dfs DirFS) Open(path string) (fs.File, error) {
 	return dfs.innerDirFS.Open(path)
+}
+
+func (dfs DirFS) Mkdir(path string, permissions fs.FileMode) error {
+	if err := checkPath(path, "mkdir"); err != nil {
+		return err
+	}
+
+	err := os.Mkdir(filepath.Join(dfs.baseDir, path), permissions)
+	if err != nil {
+		// restore the original path instead of its joined version
+		err.(*fs.PathError).Path = path
+
+		return err
+	}
+
+	return nil
+}
+
+// This check is made for consistency with the implementation of `os.DirFS()`.
+//
+// # License
+//
+//	BSD 3-Clause "New" or "Revised" License
+//	Copyright (C) 2009 The Go Authors
+func checkPath(path string, operation string) error {
+	if !fs.ValidPath(path) ||
+		(runtime.GOOS == "windows" && strings.ContainsAny(path, `\:`)) {
+		return &fs.PathError{Op: operation, Path: path, Err: fs.ErrInvalid}
+	}
+
+	return nil
 }
--- dir_fs_test.go
+++ dir_fs_test.go
@@ -113,3 +113,94 @@ func TestDirFS_Open(test *testing.T) {
 		})
 	}
 }
+
+func TestDirFS_Mkdir(test *testing.T) {
+	type args struct {
+		path        string
+		permissions fs.FileMode
+	}
+
+	for _, data := range []struct {
+		name        string
+		preparation func(test *testing.T, tempDir string)
+		args        args
+		want        func(test *testing.T, tempDir string)
+		wantErr     func(test *testing.T, tempDir string, err error)
+	}{
+		{
+			name:        "success",
+			preparation: func(_ *testing.T, _ string) {},
+			args: args{
+				path:        "path",
+				permissions: 0700,
+			},
+			want: func(test *testing.T, tempDir string) {
+				path := filepath.Join(tempDir, "path")
+				if !assert.DirExists(test, path) {
+					return
+				}
+
+				stat, err := os.Stat(path)
+				if !assert.NoError(test, err) {
+					return
+				}
+
+				assert.Equal(test, 0700|fs.ModeDir, stat.Mode())
+			},
+			wantErr: func(test *testing.T, _ string, err error) {
+				assert.NoError(test, err)
+			},
+		},
+		{
+			name:        "error/invalid path",
+			preparation: func(_ *testing.T, _ string) {},
+			args: args{
+				path:        "/invalid-path",
+				permissions: 0700,
+			},
+			want: func(_ *testing.T, _ string) {},
+			wantErr: func(test *testing.T, _ string, err error) {
+				wantPath := "/invalid-path"
+				wantErr := &fs.PathError{Op: "mkdir", Path: wantPath, Err: fs.ErrInvalid}
+				assert.Equal(test, wantErr, err)
+			},
+		},
+		{
+			name: "error/existent path",
+			preparation: func(test *testing.T, tempDir string) {
+				path := filepath.Join(tempDir, "existent-path")
+				err := os.Mkdir(path, 0700)
+				require.NoError(test, err)
+			},
+			args: args{
+				path:        "existent-path",
+				permissions: 0700,
+			},
+			want: func(_ *testing.T, _ string) {},
+			wantErr: func(test *testing.T, _ string, err error) {
+				if !assert.IsType(test, (*fs.PathError)(nil), err) {
+					return
+				}
+
+				typedErr := err.(*fs.PathError)
+				assert.Equal(test, "mkdir", typedErr.Op)
+				assert.Equal(test, "existent-path", typedErr.Path)
+				assert.ErrorIs(test, typedErr.Err, fs.ErrExist)
+			},
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			tempDir, err := os.MkdirTemp("", "test-*")
+			require.NoError(test, err)
+			defer os.RemoveAll(tempDir)
+
+			data.preparation(test, tempDir)
+
+			dfs := NewDirFS(tempDir)
+			err = dfs.Mkdir(data.args.path, data.args.permissions)
+
+			data.want(test, tempDir)
+			data.wantErr(test, tempDir, err)
+		})
+	}
+}
